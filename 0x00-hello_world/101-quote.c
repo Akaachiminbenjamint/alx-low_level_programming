e7d07b72a3d9b4a4ae6933695f749a319bd08507
Akaachiminbenjamint <theophilusbenjamin33@gmail.com>
2023-06-23 21:30:22 +0100

    0x00. C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,17 +2,20 @@
  * File: 101-quote.c
  * Author: Terfa Ben
  */
-#include <stdio.h>
+
+#include <unistd.h>
+
 /**
- * main - prints and that piece of art is useful" - Dora Korpar, 2015-10-19
- * \t followed by a new line
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * \t followed by a new line, to standard error.
+ *
  * Return: Always 1.
  */
 int main(void)
 {
 	write(2,
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 		59);
-	
+
 	return (1);
 }
