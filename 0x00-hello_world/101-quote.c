82791a6f9b6a3d8eccd5e5327425616d3ae7f6a8
Akaachiminbenjamint <theophilusbenjamin33@gmail.com>
2023-06-23 16:38:47 +0100

    0x00. C - Hello, World

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+/*
+ * File: 101-quote.c
+ * Author: Terfa Ben
+ */
+#include <stdio.h>
+/**
+ * main - prints and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * \t followed by a new line
+ * Return: Always 1.
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	
+	return (1);
+}
