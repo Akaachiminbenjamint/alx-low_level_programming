9ff40bbab7ebf53a46c338abd83abb4d6bdf5839
Akaachiminbenjamint <theophilusbenjamin33@gmail.com>
2023-06-23 15:44:48 +0100

    0x00. C - Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,12 +12,9 @@
 int main(void)
 {
 	printf("size of char: %zu bytes\n", sizeof(char));
-	printf("size of long: %zu bytes\n", sizeof(long));
-	printf("size of long long: %zu bytes\n", sizeof(long long));
-	printf("size of double: %zu bytes\n", sizeof(double));
-	printf("size of long double: %zu bytes\n", sizeof(long double));
+	printf("size of long int: %zu bytes\n", sizeof(long int));
+	printf("size of long long int: %zu bytes\n", sizeof(long long int));
 	printf("size of float: %zu bytes\n", sizeof(float));
 	printf("size of int: %zu bytes\n", sizeof(int));
-	printf("size of short: %zu bytes\n", sizeof(short));
 	return (0);
 }
